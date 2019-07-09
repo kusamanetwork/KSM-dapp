cae72b42a40aa956be93080de770f91140a8b99b
Logan Saether <x@logansaether.com>
2019-07-09 14:33:39 +0200

    On Ethereum claiming

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { decodeAddress, encodeAddress } from '@polkadot/keyring';
 import * as pUtil from '@polkadot/util';
 import React from 'react';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faGifts, faUnlink } from '@fortawesome/free-solid-svg-icons';
+import { faClipboard, faThumbsUp, faGifts, faUnlink } from '@fortawesome/free-solid-svg-icons';
 import Web3 from 'web3';
 
 import Manual from './Manual';
@@ -26,6 +27,7 @@ const Navbar = styled.div`
   justify-content: space-around;
   position: fixed;
   color: white;
+  z-index: 100;
 `;
 
 const NavButton = styled.button`
@@ -60,7 +62,7 @@ const MainLeft = styled.div`
   width: 42%;
   margin-right: 1%;
   background: rgba(255,255,255,1.0);
-  border-radius: 3px;
+  border-radius: 12px;
   padding: 2%;
   padding-top: 2%;
 `;
@@ -72,8 +74,9 @@ const MainRight = styled.div`
   width: 42%;
   margin-left: 1%;
   background: white;
-  border-radius: 3px;
+  border-radius: 12px;
   padding: 2%;
+  padding-top: 0;
 `;
 
 const MainBottom = styled.div`
@@ -85,7 +88,7 @@ const MainBottom = styled.div`
   margin-top: -1%;
   margin-bottom: 3%;
   background: white;
-  border-radius: 3px;
+  border-radius: 12px;
   padding: 2%;
 
 `;
@@ -105,8 +108,10 @@ const MyInput = styled.input`
   border-color: black;
   border-radius: 10px;
   border-width: 2px;
-  padding: 1px;
-  padding-left: 2px;
+  padding: 2px;
+  padding-left: 3px;
+  font-size: 13px;
+  margin-bottom: 16px;
 `;
 
 const MySelect = styled.select`
@@ -136,6 +141,39 @@ const MyButton = styled.button`
   }
 `;
 
+const TextareaButton = styled.button`
+  margin-top: -35px;
+  margin-left: 475px;
+  position: absolute;
+  background: black;
+  color: white;
+  border-color: black;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const DisabledText = styled.div`
+  background: #EBEBE4;
+  font-size: 12px;
+  color: #545454;
+  border-style: solid;
+  border-color: silver;
+  border-width: 0.25px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100%;
+  height: 25px;
+  overflow-x: hidden;
+  position: relative;
+`;
+
+const DisabledButton = styled.button`
+  position: absolute;
+  right: 0;
+`;
+
 
 class App extends React.Component {
 
@@ -147,6 +185,7 @@ class App extends React.Component {
     frozenToken: null,
     metamask: false,
     myCrypto: false,
+    pubKey: null,
     showAmend: false,
     status: null,
     web3: null,
@@ -186,7 +225,11 @@ class App extends React.Component {
     const { name, value } = e.target;
     if (name === 'valid-check') {
       if (value.length === 48) {
+        const pubKey = pUtil.u8aToHex(decodeAddress(value));
+        console.log(pubKey);
+
         this.setState({
+          pubKey,
           status: 'true',
         })
       } else {
@@ -318,37 +361,69 @@ class App extends React.Component {
               <Main>
                 <MainLeft>
                   <h1>Claim KSMA</h1>
+                  <br/>
                   <p>This DApp will walk you through the process of claiming KSMA. In order to claim KSMA you need to have an allocation of DOTs.</p>
                   <h2>Create a Kusama address</h2>
+                  <br/>
                   <p>You will first need to create an account. This is the account that you will be claiming your KSMA to, so make sure to extra precautions to keep it secure. For some tips on keeping your key safe, <a href='#'>see here</a>. Create an account using one of the following methods:</p>
                   <ul>
                     <li>Polkadot UI <b>(Recommended for most users)</b></li>
                     <li><code>subkey</code> <b>(Most secure)</b></li>
-                    <li>Enzyme wallet</li>
+                    <li><a href="https://chrome.google.com/webstore/detail/enzyme/amligljifngdnodkebecdijmhnhojohh" target="_blank">Enzyme wallet</a> <b>(Chrome only)</b></li>
                     <li>Polkawallet</li>
                   </ul>
+                  <br/>
                   <a href="#">Need help?</a>
 
                 </MainLeft>
                 <MainRight>
-                <h4>What is your Kusama address?</h4>
-                  <div>
-                    <MyInput
-                      width='300'
-                      name='valid-check'
-                      onChange={this.inputChange}
-                    />
-                    {' '}<SucceedIcon icon={Boolean(this.state.status) ? faCoffee : faUnlink} status={this.state.status}/>
-                  </div>
-                  <br/>
-                  <h4>How will you claim?</h4>
-                  <MySelect onChange={this.handleSelect} defaultValue="">
-                    <option value="" disabled hidden>Choose your method to claim</option>
-                    <option value="Metamask">Metamask (before genesis)</option>
-                    <option value="MyCrypto">MyCrypto (before genesis)</option>
-                    <option value="On-chain" disabled>On-chain (after genesis)</option>
-                  </MySelect>
-                  {
+                <h4>How will you claim?</h4>
+                <MySelect onChange={this.handleSelect} defaultValue="">
+                  <option value="" disabled hidden>Choose your method to claim</option>
+                  {/* <option value="Metamask">Metamask (before genesis)</option> */}
+                  <option value="MyCrypto">On Ethereum (before genesis)</option>
+                  <option value="On-chain" disabled>On Kusama (after genesis)</option>
+                </MySelect>
+                {
+                  this.state.myCrypto &&
+                    <div>
+                      <h4>Claims contract:</h4>
+                      <DisabledText>
+                        0xAf885DB79f61A08Ad3341011a293a708bff85317
+                        <CopyToClipboard text="0xAf885DB79f61A08Ad3341011a293a708bff85317">
+                          <DisabledButton>
+                            <FontAwesomeIcon icon={faClipboard}/>
+                          </DisabledButton>
+                        </CopyToClipboard>
+                      </DisabledText>
+                      <h4>ABI:</h4>
+                      <textarea style={{width: '100%', height: '100px', resize: 'none'}} disabled>{JSON.stringify(Claims.abi)}</textarea>
+                      <CopyToClipboard text={JSON.stringify(Claims.abi)}><TextareaButton>copy</TextareaButton></CopyToClipboard>
+                      <h4>What is your Kusama address?</h4>
+                      <div>
+                        <MyInput
+                          width='450'
+                          name='valid-check'
+                          onChange={this.inputChange}
+                        />
+                        {' '}<SucceedIcon icon={Boolean(this.state.status) ? faThumbsUp : faUnlink} status={this.state.status}/>
+                      </div>
+                      <p>Public Key:</p>
+                      <DisabledText>
+                        {this.state.pubKey || ''}
+                        <CopyToClipboard text={this.state.pubKey || ''}>
+                          <DisabledButton>
+                            <FontAwesomeIcon icon={faClipboard}/>
+                          </DisabledButton>
+                        </CopyToClipboard>
+                    </DisabledText>
+                      {/* <h3>MyCrypto</h3>
+                      <div>
+                        Please download and run MyCrypto desktop application locally and follow instructions <a href="#">here</a>.
+                      </div> */}
+                    </div>
+                }
+                  {/* {
                     this.state.metamask && 
                       <div>
                         <h3>Metamask</h3>
@@ -361,41 +436,22 @@ class App extends React.Component {
                           this.state.showAmend &&
                             <div>
                               <p>Which address is it?</p>
-                              <MyInput onChange={this.validateAmend}/>
-                              {' '}<SucceedIcon icon={Boolean(this.state.status) ? faCoffee : faUnlink} status={this.state.correctAmendment}/>
+                              <MyInput width="450" onChange={this.validateAmend}/>
+                              {' '}<SucceedIcon icon={Boolean(this.state.status) ? faThumbsUp : faUnlink} status={this.state.correctAmendment}/>
                             </div>
                         }
                         <MyButton
                           onClick={this.tryClaim}
                         >Claim</MyButton>
                       </div>
-                  }
-                  {
-                    this.state.myCrypto &&
-                      <div>
-                        <h3>MyCrypto</h3>
-                        <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                          <p>Are you claiming for an amended address?</p>
-                          <input type='checkbox' onChange={() => this.setState({ showAmend: !this.state.showAmend })}></input>
-                        </div>
-                        {
-                          this.state.showAmend &&
-                            <div>
-                              <p>Which address is it?</p>
-                              <MyInput onChange={this.validateAmend}/>
-                              {' '}<SucceedIcon icon={Boolean(this.state.status) ? faCoffee : faUnlink} status={this.state.correctAmendment}/>
-                            </div>
-                        }
-                        <p>Does not require in browser compatibility (send from MyCrypto)</p>
-                      </div>
-                  }
+                  } */}
                 </MainRight>
               </Main>
               <MainBottom>
                 <h2>Check your information:</h2>
                 <h4>Paste address to your DOT allocation below to check your Kusama address, index and balance:</h4>
                 <MyInput
-                  width='400'
+                  width='500'
                   name='balance-check'
                   onChange={this.balanceCheck}
                 />
