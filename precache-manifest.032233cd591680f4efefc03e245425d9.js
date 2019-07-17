5a6f4570a3f871cb4d1e5d5dee01cedc40fa1ddf
Logan Saether <x@logansaether.com>
2019-07-17 14:27:15 +0200

    Updates

---
+++ precache-manifest.032233cd591680f4efefc03e245425d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cde2730a646b8c03ed247f37523d695",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6e72dbf8dbd5648d3a7",
+    "url": "/static/css/main.f24f4f9a.chunk.css"
+  },
+  {
+    "revision": "fb5ab77e5e6b3755aa1f",
+    "url": "/static/js/2.3372f433.chunk.js"
+  },
+  {
+    "revision": "b6e72dbf8dbd5648d3a7",
+    "url": "/static/js/main.4b7f4c03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "78db67b9eb67c617b250dec73bc52991",
+    "url": "/static/media/Visuelt_Bold.78db67b9.ttf"
+  },
+  {
+    "revision": "38dc0755e8db6bca8a19e6f8ecdb27fe",
+    "url": "/static/media/claiming.38dc0755.md"
+  },
+  {
+    "revision": "3e581abcc5d32af901a58ab981c27c2d",
+    "url": "/static/media/kusama_word.3e581abc.png"
+  },
+  {
+    "revision": "bea51fd0415d64428ec0f642f0d751b3",
+    "url": "/static/media/overview.bea51fd0.md"
+  },
+  {
+    "revision": "463f2fcbc0ed999a6cc3a67fe0990860",
+    "url": "/static/media/security.463f2fcb.md"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.569f948d127b22fca4538610ae5ceaf5.js"
+  "/precache-manifest.032233cd591680f4efefc03e245425d9.js"
 );
 
 self.addEventListener('message', (event) => {
